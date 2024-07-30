2b115aa0acef33cb76a9a36b07ad6b191f500f0f
Nomad <imshnv2@gmail.com>
2024-07-30 16:47:10 +0400

    step 5

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 [![Actions Status](https://github.com/nmdcat/fullstack-javascript-project-44/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/nmdcat/fullstack-javascript-project-44/actions)
 
 <a href="https://codeclimate.com/github/nmdcat/fullstack-javascript-project-44/maintainability"><img src="https://api.codeclimate.com/v1/badges/84b3c14d76085067c804/maintainability" /></a>
+
+https://asciinema.org/a/ztOHFtsYK01JMvVVnor0zDquT
--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,21 @@
 import readlineSync from 'readline-sync';
 
+let userName = "";
+
+export function greeting() {
+    console.log("Welcome to the Brain Games!");
+}
+
+export function getUserName() {
+  if (!userName) {
+    userName = readlineSync.question("May I have your name? ");
+  }
+  return userName;
+}
+
 export default function greetUser() {
-    const name = readlineSync.question("May I have your name? ");
+    greeting();
+    const name = getUserName();
     console.log(`Hello, ${name}!`);
+    return name;
 }
